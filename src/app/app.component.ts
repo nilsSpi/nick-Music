9a29fcc0fe3d67f7b43960b9f45615b49639f046
nils <nils.christian@web.de>
2021-12-14 20:57:50 +0100

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  
+
+   images=[
+     './assets/img/dj1.jpg',
+     './assets/img/dj2.jpg',
+     './assets/img/dj3.jpg',
+     './assets/img/dj4.jpg',
+     './assets/img/dj5.jpg',
+     './assets/img/dj6.jpg',
+     './assets/img/dj7.jpg',
+   ];
+   currentImage=0;
+   showImage = true;
+
+   ngOnInit() {
+     this.updateImage();
+
+   }
+
+   updateImage(){
+     setInterval(() =>{
+       this.currentImage++;
+       this.currentImage = this.currentImage % this.images.length;
+       this.showImage=false;
+
+
+       setTimeout(() => {
+        this.showImage=true;
+       }, 1);
+     },8000);
+
+    
+   }
+
+
+
+
+
+
+
+
+
+
 }
